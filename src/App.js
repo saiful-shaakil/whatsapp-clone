531c2064ee11d1e0ebceb9368d77ccc1f80ede82
si-shakil <sishakil850@gmail.com>
2022-06-20 19:58:04 +0600

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-body">
+      <div className="main-body"></div>
+    </div>
+  );
+}
+
+export default App;
