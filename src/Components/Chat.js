14e785f32ef94e72312e950f72a4ed90e8b26f09
si-shakil <sishakil850@gmail.com>
2022-06-22 23:24:14 +0600

    chat footer section updated

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,8 +1,17 @@
-import { AttachFile, MoreVert, SearchOutlined } from "@mui/icons-material";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
 import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 import "./Chat.css";
 const Chat = () => {
+  const sendMessage = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="chat">
       <div className="chat-header">
@@ -24,11 +33,21 @@ const Chat = () => {
         </div>
       </div>
       <div className="chat-body">
-        <p className="chat-message">Hey Gusys</p>
-        <p className="chat-message">Hey Gusys</p>
-        <p className="chat-message">Hey Gusys</p>
+        <p className={`chat-message ${true && "chat-receiver"}`}>
+          <span className="chat-name">Shahin Abrar</span>
+          Hey Guys <span className="chat-time">5:32 pm</span>
+        </p>
       </div>
-      <div className="chat-footer"></div>
+      <div className="chat-footer">
+        <InsertEmoticon />
+        <form>
+          <input type="text" placeholder="Type a message" name="" id="" />
+          <button type="submit" onClick={sendMessage}>
+            Send a message
+          </button>
+        </form>
+        <Mic />
+      </div>
     </div>
   );
 };
